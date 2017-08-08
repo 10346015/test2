a53c84a6ece0cdd477b3ed3184a48a25e230f3de
Sherly <sherly.chang@tapbee.cc>
2017-08-08 10:01:16 +0800

    register phonenumber validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        Validator::extend('phone_number', function ($attribute, $value, $parameters, $validator) {
+          if(preg_match("/^0[9]\d{8}$/", $value)){
+          return true;
+          }
+          return false;
+        });
+
     }
 
     /**
--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -13,6 +13,7 @@ return [
     |
     */
 
+    'phone_number'         => 'Phone number format is wrong.',
     'accepted'             => 'The :attribute must be accepted.',
     'active_url'           => 'The :attribute is not a valid URL.',
     'after'                => 'The :attribute must be a date after :date.',
